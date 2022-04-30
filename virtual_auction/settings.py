926b0f5e33de05a2fb7e862fa2fd7a5c88eb1ab7
pavankumarvm <pavankumarmaurya1999@gmail.com>
2022-04-30 15:58:43 +0530

    static file issue

--- virtual_auction/settings.py
+++ virtual_auction/settings.py
@@ -135,6 +135,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets/')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static/'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
